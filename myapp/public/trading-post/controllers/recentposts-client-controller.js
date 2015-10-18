23eb6e6974d92454ed78156b28bf32e2bae8948b
JESSICA <jsobrique@yahoo.com>
2015-10-18 13:38:19 -0700

    Updated Recent Posts

--- myapp/public/trading-post/controllers/recentposts-client-controller.js
+++ myapp/public/trading-post/controllers/recentposts-client-controller.js
@@ -5,18 +5,35 @@ angular.module('trading-post').controller('recentController', function($scope, $
 
 	$scope.name ='recent';
 
-	$scope.recentPosts = [];
+	$scope.recentPosts = {};
+
+	/**$scope.recentPosts = [{				
+				owner: "Mana",
+				zipcode: "90277",	
+				name: "Oranges",
+				type: "Good",
+				quantity: "5",
+				imgUrl: "./img/haircut_sample.png",
+				category: "Food",
+				datePosted: (new Date()).toString()},
+
+				{owner: "Mana",
+				zipcode: "90277",	
+				name: "Oranges",
+				type: "Good",
+				quantity: "5",
+				imgUrl: "./img/haircut_sample.png",
+				category: "Food",
+				datePosted: (new Date()).toString()
+			}];**/
 
 	$scope.requestRecentPosts = function() {
 
 		$http.post('/recent').
 		   success(function(responseData) {
 
-			var i;
-			for( i = 0; i < responseData.length; i++ ){
-				$scope.recentPosts[i] = responseData[i];
-				console.log("res: ",responseData[i]);}
-    		   }).
+				$scope.recentPosts = responseData;
+			}).
 		   error(function(responseData) {
     	   		 console.log('Recent posts POST error. Received: ', responseData);
     	  	 });
